d6d357bdd8e422d4e8e6a25586c15a7dd70476ab
Zivess <110631271+Zivess@users.noreply.github.com>
2022-12-13 13:38:23 -0300

    Limpeza arquivos desnecessarios

--- MODELOS HTML + CSS/modelo MENU ACCORDION/menu accordion.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".item-principal-menu").click(function(){ $(".sub-menu").slideUp(400) });
-  });
-  
-  $(document).ready(function(){
-    $(".item-principal-menu").click(function(){ if ($(this).next().is(":visible")){
-      $(this).next().slideUp(400)
-    }else{ $(this).next().slideDown(400) } })
-    });
